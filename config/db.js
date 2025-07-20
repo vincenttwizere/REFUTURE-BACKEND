ac3b0da616cd1872b3743613270494ae7ee1964e
vincenttwizere <v.twizere@alustudent.com>
2025-07-20 22:37:45 +0200

    controller and routes updates

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/refuture';
+const MONGODB_URI = process.env.MONGODB_URI 
 
 const connectDB = async () => {
   try {
